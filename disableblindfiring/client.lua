7071317ab45af226ca679c9f3dc2e28f4e3816b0
BahBROOOT <73172839+BahBROOOT@users.noreply.github.com>
2023-05-02 23:00:12 +0200

    Add files via upload

---
+++ disableblindfiring/client.lua
@@ -0,0 +1,19 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local player = GetPlayerPed(-1)
+        if IsPedInCover(player) then
+            DisableControlAction(0, 24, true) -- disable attack
+            DisableControlAction(0, 25, true) -- disable aim
+            DisableControlAction(0, 47, true) -- disable weapon
+            DisableControlAction(0, 58, true) -- disable weapon
+            DisableControlAction(0, 263, true) -- disable melee
+            DisableControlAction(0, 264, true) -- disable melee
+            DisableControlAction(0, 257, true) -- disable melee
+            DisableControlAction(0, 140, true) -- disable melee
+            DisableControlAction(0, 141, true) -- disable melee
+            DisableControlAction(0, 142, true) -- disable melee
+            DisableControlAction(0, 143, true) -- disable melee
+        end
+    end
+end)
